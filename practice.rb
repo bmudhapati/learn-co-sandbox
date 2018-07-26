50942d9dbec5586898063681d5fb99d371e85fb9
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 16:01:36 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,7 +1,7 @@
 snacks = []
 snacks = ["dried mango", "bananas", "cookies"]
   snacks.each do |snacks|
-  puts "#{snacks} is one of my favorite snacks."
+  puts "#{snacks.last} is one of my favorite snacks."
   # puts snacks.size 
   # puts snacks.first
   # puts snacks[0]
