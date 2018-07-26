c2b1ab3ae8ba1ff648282cbca0de7c077308aa1e
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 16:55:34 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,6 +1,6 @@
 shopping_hash = {"24k_magic" => 15.98, "crocs" => 35, "iphone" => 800.50, "ACT prep book" => 15}
 shopping_hash.each do |item_name, price|
-  puts "#{item_name}: #{price}"
+  puts item_name, price
 end
 
 
