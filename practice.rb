400e323349b0333fc659a10b35e6424521bfdb9f
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 15:56:31 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,3 +1,7 @@
-puts "What is your name"
-  gets
-  
+snacks = []
+snacks = ["dried mango", "bananas", "cookies"]
+  puts "#{snacks} is one of my favorite snacks."
+  puts snacks.size 
+  puts snacks.first
+  puts snacks.last
+end
