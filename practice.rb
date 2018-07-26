ae9d78a663556b426d1531e27825860a946c2c25
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 16:08:47 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,8 +1,2 @@
-snacks = []
-snacks = ["dried mango", "bananas", "cookies"]
-  snacks.each do |snacks|
-  puts "#{snacks.last} is one of my favorite snacks."
-  # puts snacks.size 
-  # puts snacks.first
-  # puts snacks[0]
-end
+has_name = {key => value}
+  puts hash_name
