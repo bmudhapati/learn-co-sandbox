404509110fadc7c627ad93e8d721ecc02286ba22
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 16:44:51 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,4 +1,4 @@
-items = { "24k_magic" => 15.98, "crocs" => 35}
-  items["iphone"] = 800.50
-  items["ACT prep book"] = 15
-puts items
+shopping_hash = {"24k_magic" => 15.98, "crocs" => 35, "iphone" => 800.50, "ACT prep book" => 15}
+puts shopping_hash.keys 
+
+
